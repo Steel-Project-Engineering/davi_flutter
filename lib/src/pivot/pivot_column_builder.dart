9bcd2aa40be52fc0f1a599361d60dc71268e011b
Mariolismo <marioilsimo94@gmail.com>
2025-04-08 15:45:47 +0200

    Amend previous commit

--- lib/src/pivot/pivot_column_builder.dart
+++ lib/src/pivot/pivot_column_builder.dart
@@ -75,9 +75,7 @@ class PivotColumnBuilder<T, L extends HierarchyLevel> {
                   icon: Icon(
                     isExpanded ? Icons.expand_more : Icons.chevron_right,
                     size: 20,
-                    color: Theme.of(params.buildContext).brightness == Brightness.dark
-                        ? Colors.white70  // lighter color for dark theme
-                        : null,  // default color for light theme
+                    color: Colors.grey[400],  // matches typical table border color
                   ),
                   onPressed: () => model.toggleExpand(params.rowIndex),
                   padding: EdgeInsets.zero,
